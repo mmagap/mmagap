63b974fbdebff7569b5af4e725a35f4878c9c6b8
root <root@vertax12.(none)>
2014-03-06 18:23:02 +0530

    Improve the README file

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -9,9 +9,9 @@ ActiveAdmin.setup do |config|
   config.site_title = "Mmagap"
  
  
-  config.register_javascript "jwplayer/jwplayer"
-  config.register_javascript "jwplayer/my"
-  config.register_javascript "ckeditor/ckeditor"
+  #config.register_javascript "jwplayer/jwplayer"
+  #config.register_javascript "jwplayer/my"
+  #config.register_javascript "ckeditor/ckeditor"
   #config.register_javascript 'ckeditor/config.js'
   #config.register_javascript 'ckeditor/ckeditor.js' 
   #config.register_javascript 'ckeditor/init.js'
@@ -19,7 +19,7 @@ ActiveAdmin.setup do |config|
   
   #config.view_factory.footer = Footer
 
-  config.site_title = "MMAGAP.COM"
+  #config.site_title = "MMAGAP.COM"
 
 
   # Set the link url for the title. For example, to take
