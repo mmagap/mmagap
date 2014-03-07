f9e532b4d0d06dc20be4af677202b9bb0f08ac55
root <root@vertax12.(none)>
2014-03-07 19:21:58 +0530

    Improve the README file

--- app/helpers/active_admin/views_helper.rb
+++ app/helpers/active_admin/views_helper.rb
@@ -17,7 +17,9 @@ module ActiveAdmin::ViewsHelper #camelized file name
               <div id="myElement">Loading the player...</div>
               <script type="text/javascript">
                 jwplayer("myElement").setup({
+                    flashplayer: '/assets/jwplayer.flash.swf"',
                     file: "#{youtube_url}"
+                    base:'/assets/',
                 });
               </script>
             EOHTML
